b44400f22fdb519feb52d14ed4875d95bc21c930
Scott Mebberson <scott@scottmebberson.com>
2019-03-25 20:52:29 +1030

    First version of the site.

---
+++ _next/static/chunks/1.js
@@ -0,0 +1,32 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[1],{
+
+/***/ "./posts.js":
+/*!******************!*\
+  !*** ./posts.js ***!
+  \******************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+module.exports = {
+  posts: [{
+    color: 'deepPink',
+    date: '2019-01-05',
+    slug: 'first-post',
+    teaser: 'This is the teaser for the first post',
+    title: 'First post'
+  }, {
+    color: 'green',
+    date: '2019-02-10',
+    slug: 'second-post',
+    teaser: 'This is the teaser for the second post',
+    title: 'Second post title'
+  }]
+};
+
+/***/ })
+
+}]);
+//# sourceMappingURL=1.js.map
---
+++ _next/static/chunks/10.b6f5daff35c50c9ccca5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{I6xX:function(t,s,e){"use strict";t.exports={posts:[{color:"deepPink",date:"2019-01-05",slug:"first-post",teaser:"This is the teaser for the first post",title:"First post"},{color:"green",date:"2019-02-10",slug:"second-post",teaser:"This is the teaser for the second post",title:"Second post title"}]}}}]);
