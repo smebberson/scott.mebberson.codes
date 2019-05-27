7003b8e949f504be17925192b60032887eb5a924
Scott Mebberson <scott@scottmebberson.com>
2019-05-27 12:22:56 +0000

    Deploying commit 804599d94ffbf0715fcd5072fa591d5208733914 to gh-pages 🚀

--- _next/static/chunks/12.13e37e886586f999fb94.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{I6xX:function(e,s,t){"use strict";e.exports={posts:[{color:"deepPink",date:"2019-03-25",slug:"next-js-on-gh-pages",teaser:"A simple guide to hosting a Next.js site on GitHub pages.",title:"Next.js on gh-pages"}]}}}]);
