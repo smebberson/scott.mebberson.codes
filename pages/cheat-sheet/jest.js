ce9c857ecdfc87106897535dbd2671e8b0f7c73c
Scott Mebberson <scott@scottmebberson.com>
2019-06-01 15:11:32 +0930

    Set the language to JavaScript for syntax highlighting.

--- pages/cheat-sheet/jest.js
+++ pages/cheat-sheet/jest.js
@@ -11,21 +11,25 @@ export default () => (
         <Cheat
             caption="Create a mock function."
             code="const mockFn = jest.fn()"
+            language="javascript"
         />
 
         <Cheat
             caption="Assert the number of times the mock function has been called."
             code="expect(mockFn.mock.calls.length).toBe(2);"
+            language="javascript"
         />
 
         <Cheat
             caption="Assert the value of the first argument the first time the mock function was called."
             code="expect(mockFn.mock.calls[0][1]).toBe(action);"
+            language="javascript"
         />
 
         <Cheat
             caption="Set the return value of the mock function."
             code="expect(mockFn.mockReturnValueOnce(false).mockReturnValue(true);"
+            language="javascript"
         />
         
     </CheatSheet>
