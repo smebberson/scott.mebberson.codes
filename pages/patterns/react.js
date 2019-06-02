bf0850e3cfd9939675ccb780f98b5a90fc5bac35
Scott Mebberson <scott@scottmebberson.com>
2019-06-02 15:53:38 +0930

    Added a react pattern.

--- pages/patterns/react.js
+++ pages/patterns/react.js
@@ -1,4 +1,5 @@
 import Heading from '../../components/Heading';
+import Inline from '../../components/Inline';
 import Page from '../../components/Page';
 import Pattern from '../../components/Pattern';
 import { react } from '../../lib/colours';
@@ -27,6 +28,18 @@ export default () => (
 { caption ||
     <p>Default caption.</p>
 }
+            `}
+            language="jsx"
+        />
+
+        <Heading>JSX</Heading>
+
+        <Pattern
+            caption={
+                <span>Because you can't use JSX within a string, wrap the strings in <Inline>span</Inline> components.</span>
+            }
+            code={`
+{<><span>Surround any strings using the</span><Inline>span</Inline><span> component.</span></>}
             `}
             language="jsx"
         />
