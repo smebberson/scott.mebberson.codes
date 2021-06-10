08525223508e3f07de18095cc03c34a983f8af4a
Scott Mebberson <scott@scottmebberson.com>
2021-06-10 11:26:43 +0930

    Updated the About page.

--- pages/about.js
+++ pages/about.js
@@ -6,15 +6,6 @@ export default () => (
         <PageTitle emojis="👨‍💻">About</PageTitle>
 
         <div className="constrained">
-            <p>Hey 👋.</p>
-            <p>
-                If you want to read more about me, as opposed to this website,
-                head over to{' '}
-                <a href="https://www.scottmebberson.com" target="_blank">
-                    scottmebberson.com
-                </a>
-                .
-            </p>
             <p>
                 This site is a collection of articles, cheat sheets and patterns
                 that I use when developing.
