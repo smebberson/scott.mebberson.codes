f81ca7520e008bb6391a3d638f6578de2b0d0223
Scott Mebberson <74628+smebberson@users.noreply.github.com>
2023-03-27 12:33:34 +1030

    Added a kubernetes cheat.

--- pages/cheat-sheet/kubernetes.js
+++ pages/cheat-sheet/kubernetes.js
@@ -8,5 +8,9 @@ export default () => (
             caption="Set the namespace for the minikube context to an empty string"
             code="$ kubectl config set-context minikube --namespace="
         />
+        <Cheat
+            caption="Download a file from a remote pod to your local machine"
+            code="$ kubectl cp {namespace}/{pod}:{remote-file} {local-file}"
+        />
     </CheatSheet>
 );
