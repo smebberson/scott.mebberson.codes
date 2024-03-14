841f5c6099c6bf10a296e11a831c3c8b56dcf477
Scott Mebberson <74628+smebberson@users.noreply.github.com>
2024-03-15 08:56:42 +1030

    Added some date rendering pattern to React.

--- pages/patterns/react.js
+++ pages/patterns/react.js
@@ -51,5 +51,41 @@ export default () => {
             }
             language="jsx"
         />
+
+        <Heading>Dates</Heading>
+
+        <p>
+            Rendering dates in React is not always straight forward, especially
+            when you throw SSR into the mix. As a default rule, keep dates in
+            UTC and then render them on the client to ensure they pick up the
+            viewers locale and are rendered appropriately.
+        </p>
+
+        <p>
+            Alter this general rule as required such as opening hours for a
+            business; they should always be rendered within context of the
+            timezone of which the business operates.
+        </p>
+
+        <Pattern
+            code={`
+export default function IntlDate({ date, locales, options = {} }) {
+    const [formattedDate, setFormattedDate] = useState(null);
+
+    useEffect(() => {
+        setFormattedDate(new Intl.DateTimeFormat(locales, options).format(date));
+    }, [date, locales, options]);
+
+    return <span>{formattedDate}</span>;
+}
+            `}
+            caption={
+                <span>
+                    Render a UTC date on the client to ensure it picks up the
+                    clients time zone and correctly outputs the date.
+                </span>
+            }
+            language="jsx"
+        />
     </Page>
 );
