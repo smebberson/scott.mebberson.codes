cd918ce75fd54044c278fdb074b8d35ec8cdd67f
Scott Mebberson <74628+smebberson@users.noreply.github.com>
2024-03-15 09:12:31 +1030

    Updates to the Cloudflare maintenance page pattern to use data URIs.

--- pages/patterns/cloudflare.js
+++ pages/patterns/cloudflare.js
@@ -1,6 +1,4 @@
-import SubHeading from '../../components/SubHeading';
 import Heading from '../../components/Heading';
-import Inline from '../../components/Inline';
 import Page from '../../components/Page';
 import Pattern from '../../components/Pattern';
 import { cloudflare } from '../../lib/colours';
@@ -22,11 +20,9 @@ export default () => (
         </p>
         <ul>
             <li>
-                Requests from developers will still go through to the origin.
+                Requests from allowed IP list will go through to the origin.
             </li>
-            <li>
-                Specific requests (for things like logos) can still be allowed.
-            </li>
+            <li>Uses data URIs for the favicon and logo to reduce requests.</li>
         </ul>
 
         <Pattern
@@ -37,6 +33,9 @@ const html = (ip, url) => \`
   <html>
     <head>
       <title>Maintenance</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width; initial-scale=1.0">
+      <link rel="icon" href="data:image/vnd.microsoft.icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAHBwcAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEREREREREREQAAAAAAAAARAAAAAAAAABEAAAAAAAAAEQAAAAAAAAARAAAAAAAAABEAAAEREAAAEQAAAREQAAARAAABERAAABEAAAEREAAAEQAAAAAAAAARAAAAAAAAABEAAAAAAAAAEQAAAAAAAAARAAAAAAAAABEREREREREREAAAAAf/4AAH/+AAB//gAAf/4AAH/+AAB8PgAAfD4AAHw+AAB8PgAAf/4AAH/+AAB//gAAf/4AAH/+AAAAAAAA">
       <style>
       html, body {
         font-family: sans-serif;
@@ -49,23 +48,20 @@ const html = (ip, url) => \`
     </head>
     <body>
       <!-- ip: \${ip}; url: \${url}; -->
-      <img src="https://domain.com/img/logo.png" width="200px">
+      <img alt="logo" width="100" height="100" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAYAAABw4pVUAAAAAXNSR0IArs4c6QAAAjFJREFUeF7tnEFqA1EUwyb3P3TLdDcQamKw+ARl7WdnpLRZta/run4uX8cQeCnkGBd/b0QhZ/lQyGE+3gq5f2p8cQQe3+HvfmUphJNxLymE5R3XFBIRsQGFsLzjmkIiIjagEJZ3XFNIRMQGFMLyjmsKiYjYgEJY3nFNIRERG1AIyzuuKSQiYgMKYXnHNYVERGxAISzvuKaQiIgNKITlHdcUEhGxAYWwvOOaQiIiNqAQlndcU0hExAYUwvKOawqJiNiAQljecU0hEREbUAjLO64pJCJiAwphecc1hUREbEAhLO+4ppCIiA0ohOUd1xQSEbEBhbC845pCIiI2oBCWd1xTSETEBhTC8o5rXyPk8SD/PPbpf3evkPiZZQMKYXnHNYVERGxAISzvuKaQiIgNKITlHdcUEhGxAYWwvOOaQiIiNvA1QlhsuzWF7NhWzQqpsO2OFLJjWzUrpMK2O1LIjm3VrJAK2+5IITu2VbNCKmy7I4Xs2FbNCqmw7Y4UsmNbNSukwrY7UsiObdWskArb7kghO7ZVs0IqbLsjhezYVs0KqbDtjhSyY1s1K6TCtjtSyI5t1ayQCtvuSCE7tlWzQipsuyOF7NhWzQqpsO2OFLJjWzUrpMK2O1LIjm3VrJAK2+5IITu2VbNCKmy7I4Xs2FbNCqmw7Y4UsmNbNSukwrY7UsiObdWskArb7kghO7ZVs0IqbLujKGQ3bXMkcP8/woeheGFgSkAhU7yflyvkc2bTi19RucoBR8iq5AAAAABJRU5ErkJggg=="/>
       <h1>We'll be back soon</h1>
       <p>We are currently performing some maintenance.</p>
     </body>
   </html>
 \`;
 
+const allowedIps = ['<your-ip>'];
+
 export default {
   async fetch(request, env) {
 
     // Allow requests for developers.
-    if (request.headers.get('cf-connecting-ip') === '<your-ip>') {
-      return fetch(request);
-    }
-
-    // Allow everyone to see the logo.
-    if (request.url.includes('logo.png')) {
+    if (allowedIps.includes(request.headers.get('cf-connecting-ip'))) {
       return fetch(request);
     }
 
