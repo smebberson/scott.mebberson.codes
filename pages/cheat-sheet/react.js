3a89331f974bec42634dd1baabbb5efb10e35079
Scott Mebberson <74628+smebberson@users.noreply.github.com>
2024-03-15 08:32:41 +1030

    Re-organised React moving some information from patterns to cheat sheets.

--- pages/cheat-sheet/react.js
+++ pages/cheat-sheet/react.js
@@ -2,21 +2,50 @@ import CheatSheet from '../../components/CheatSheet';
 import Cheat from '../../components/Cheat';
 import Inline from '../../components/Inline';
 import { react } from '../../lib/colours';
+import Heading from '../../components/Heading';
 
 export default () => (
     <CheatSheet color={react} title="React">
+        <Heading>Conditional rendering</Heading>
+
         <Cheat
-            caption={
-                <>
-                    <span>Using </span>
-                    <Inline>// eslint-disable-next-line rule</Inline>
-                    <span> with react.</span>
-                </>
-            }
+            caption="Conditional rendering when truthy."
             code={`
-{ // eslint-disable-next-line operator-linebreak
-} { result &&
-    <p>{result}</p>
+{ caption &&
+    <p>{caption}</p>
+}
+            `}
+            language="jsx"
+        />
+
+        <Cheat
+            caption="Conditional rendering when falsy."
+            code={`
+{ caption ||
+    <p>Default caption.</p>
+}
+            `}
+            language="jsx"
+        />
+
+        <Cheat
+            caption="Conditional rendering."
+            code={`
+{ caption && <p>{caption}</p> ||
+    <p>Default caption.</p>
+}
+            `}
+            language="jsx"
+        />
+
+        <Heading>JSX</Heading>
+
+        <Cheat
+            caption={<span>Optionally pass props to components.</span>}
+            code={`
+export default () => {
+    const [fixToTop, setFixToTop] = useState(false);
+    return <Nav { ...(fixToTop && { style: { top: '0px' } }) } />
 }
             `}
             language="jsx"
--- pages/patterns/react.js
+++ pages/patterns/react.js
@@ -6,64 +6,6 @@ import { react } from '../../lib/colours';
 
 export default () => (
     <Page color={react} title="React">
-        <Heading>Conditional rendering</Heading>
-
-        <Pattern
-            caption="Conditional rendering when truthy."
-            code={`
-{ caption &&
-    <p>{caption}</p>
-}
-            `}
-            language="jsx"
-        />
-
-        <Pattern
-            caption="Conditional rendering when falsy."
-            code={`
-{ caption ||
-    <p>Default caption.</p>
-}
-            `}
-            language="jsx"
-        />
-
-        <Pattern
-            caption="Conditional rendering."
-            code={`
-{ caption && <p>{caption}</p> ||
-    <p>Default caption.</p>
-}
-            `}
-            language="jsx"
-        />
-
-        <Heading>JSX</Heading>
-
-        <Pattern
-            caption={
-                <span>
-                    Because you can't use JSX within a string, wrap the strings
-                    in <Inline>span</Inline> components.
-                </span>
-            }
-            code={`
-{<><span>Surround any strings using the</span><Inline>span</Inline><span> component.</span></>}
-            `}
-            language="jsx"
-        />
-
-        <Pattern
-            caption={<span>Optionally pass props to components.</span>}
-            code={`
-export default () => {
-    const [fixToTop, setFixToTop] = useState(false);
-    return <Nav { ...(fixToTop && { style: { top: '0px' } }) } />
-}
-            `}
-            language="jsx"
-        />
-
         <Heading>Dynamic tag names</Heading>
 
         <Pattern
